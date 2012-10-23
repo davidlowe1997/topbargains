4e1535f8b1026a63e5df5b591cd331945e1b46d9
bbarin <brunobarin@gmail.com>
2012-10-23 14:35:36 +0200

    Folder structure refactor

---
+++ bargains-model/src/main/java/org/topbargains/model/User.java
@@ -0,0 +1,42 @@
+package org.topbargains.model;
+
+import java.util.Calendar;
+import java.util.Date;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.PrimaryKey;
+import javax.validation.constraints.NotNull;
+
+/**
+ * Represents an user registered in the system
+ * @author Bruno Barin
+ */
+@PersistenceCapable
+public class User {
+    /** User's email */
+    @PrimaryKey
+    @NotNull
+    private String email;
+    /** The date user registered in the system */
+    private Date memberSince;
+
+    /**
+     * User's constructor
+     * @param email The email identifying the user
+     */
+    public User(String email) {
+        this.email = email;
+        this.memberSince = Calendar.getInstance().getTime();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getMemberSince() {
+        return memberSince;
+    }
+}
